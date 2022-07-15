b9e99d77874e112de204bec7330ee099b0bbe130
mozhenghua <baisui@qlangtech.com>
2022-07-15 22:14:19 +0800

    add greeplum

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
  mvn deploy -pl chunjun-connectors/chunjun-connector-mysql\
 ,chunjun-connectors/chunjun-connector-clickhouse\
+,chunjun-connectors/chunjun-connector-postgresql\
+,chunjun-connectors/chunjun-connector-greenplum\
 ,chunjun-connectors/chunjun-connector-elasticsearch7 -am \
  -Dmaven.test.skip=true -DaltDeploymentRepository=base::default::http://localhost:8080/release
